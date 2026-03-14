04eb5a53b45ef85ddb079dd9bf482e9c963109cd
ThiliniNiluka <thilininilukakumari@gamil.com>
2026-03-15 04:32:14 +0530

    chore: update frontend dependencies and add new packages for improved styling and functionality

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,12 +1,27 @@
-import type { Route } from "./+types/home";
+import { Button } from "~/components/ui/button";
+import type { Route } from "../+types/root";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "TaskHub" },
+    { name: "description", content: "Welcome to TaskHub!" },
   ];
 }
 
-export default function Home() {
-  return <div>Hi</div>;
-}
+const HomePage = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center gap-4">
+      <Link to="/sign-in">
+        <Button className="bg-blue-500 text-white">Login</Button>
+      </Link>
+      <Link to="/sign-up">
+        <Button variant="outline" className="bg-blue-500 text-white">
+          Sign Up
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default HomePage;
